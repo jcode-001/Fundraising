6c175bcdece9670c12070ae8082829beafae4c31
Olaleye Tobi <olaleyetobi932@gmail.com>
2023-10-28 13:16:20 +0100

    new changes made

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,24 @@
-'use strict';
-
-/**
- * navbar variables
- */
+"use strict";
+// Navbar variables
 const menuToggleBtn = document.querySelector("[data-navbar-toggle-btn]");
 const navbar = document.querySelector("[data-navbar]");
 
-/**
- * element toggle function
- */
+// /Element toggle function
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
-const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+menuToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(navbar);
+});
 
-menuToggleBtn.addEventListener("click", function () { elemToggleFunc(navbar); });
-
-
-
-
-/**
- * go to top
- */
-
+// Go to top btn
 const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
-
   if (window.scrollY >= 800) {
     goTopBtn.classList.add("active");
   } else {
     goTopBtn.classList.remove("active");
   }
-
 });
