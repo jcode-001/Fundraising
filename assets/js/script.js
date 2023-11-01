7d9e9fe1d0fef30ca6945bd19b13aa1cab94a402
Olaleye Tobi <olaleyetobi932@gmail.com>
2023-11-01 01:13:20 +0100

    modify code, background image for home page changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,43 @@ window.addEventListener("scroll", function () {
   }
 });
 
-//
+// Carousel
+var slideIndex = 0;
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+screen.availWidth === 600
+  ? showSlides()
+  : console.log("Screen size is not up to 600px yet");
+
+// Blog Data
+const blogData = {
+  post1: {
+      title: "Blog Post 1",
+      content: "Content for Blog Post 1...",
+      image: "https://example.com/image1.jpg"
+  },
+  post2: {
+      title: "Blog Post 2",
+      content: "Content for Blog Post 2...",
+      image: "https://example.com/image2.jpg"
+  },
+  // Add more blog posts...
+};
