13ecf588b2bbc79956d04f300410ca4b615e49aa
Olaleye Tobi <olaleyetobi932@gmail.com>
2023-11-03 00:55:03 +0100

    fetch and store blogData for the users fundraising post

---
+++ assets/js/blogData.js
@@ -0,0 +1,22 @@
+// Blog Data
+export const blogData = {
+  post1: {
+    title: "Help me recover from unidentified rare disease",
+    content:
+      "Hello everyone! My name is Dora,Im from Hungary. Well,it is not easy to start...I have",
+    image: "assets/images/war__gaza-war.jpg",
+  },
+  post2: {
+    title: "Help me recover from unidentified rare disease",
+    content:
+      "Hi everyone! My name is Dora,Im from Hungary. Well,it is not easy to start...I have",
+    image: "assets/images/war__gaza-war.jpg",
+  },
+  post3: {
+    title: "Help me recover from unidentified rare disease",
+    content:
+      "Hi everyone! My name is Dora,Im from Hungary. Well,it is not easy to start...I have",
+    image: "assets/images/war__gaza-war.jpg",
+  },
+  // Add more blog posts...
+};
