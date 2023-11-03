fefb9f1aa686268abaeb49ff86add476b0be98f2
Olaleye Tobi <olaleyetobi932@gmail.com>
2023-11-03 02:10:36 +0100

    add image CDN

--- assets/js/blogData.js
+++ assets/js/blogData.js
@@ -1,16 +1,18 @@
 // Blog Data
 export const blogData = {
   post1: {
-    title: "Help me recover from unidentified rare disease",
+    title: "Israeli forces have encircled the Hamas stronghold of Gaza City",
     content:
-      "Hello everyone! My name is Dora,Im from Hungary. Well,it is not easy to start...I have",
-    image: "assets/images/war__gaza-war.jpg",
+      "The situation in Gaza has reached a catastrophic tipping point.....",
+    image:
+      "https://ichef.bbci.co.uk/news/976/cpsprodpb/A35C/production/_131602814_mediaitem131602813.jpg.webp",
   },
   post2: {
     title: "Help me recover from unidentified rare disease",
     content:
       "Hi everyone! My name is Dora,Im from Hungary. Well,it is not easy to start...I have",
-    image: "assets/images/war__gaza-war.jpg",
+    image:
+      "https://assets.gogetfunding.com/wp-content/uploads/2022/08/7643030/img/cimg_7643030_1661086740.png",
   },
   post3: {
     title: "Help me recover from unidentified rare disease",
